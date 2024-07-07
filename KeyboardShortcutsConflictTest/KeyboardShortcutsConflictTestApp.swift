689944f7d2e160604ef0598c94981903b0f72280
Michel Storms <michelstorms@icloud.com>
2024-07-07 15:30:13 +0200

    Initial Commit

---
+++ KeyboardShortcutsConflictTest/KeyboardShortcutsConflictTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardShortcutsConflictTestApp.swift
+//  KeyboardShortcutsConflictTest
+//
+//  Created by Michel Storms on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardShortcutsConflictTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
